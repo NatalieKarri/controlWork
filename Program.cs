4b809cf62fdbb9279d361a8412404c7a1e6cc150
nataliekarri <nataliekarri@gmail.com>
2022-09-17 01:20:30 +0300

    write and show array

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿void ShowArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+Console.Clear();
+Console.Write("Введите элементы массива через пробел: ");
+
+string[] array = Console.ReadLine().Split(' ',StringSplitOptions.RemoveEmptyEntries);
+ShowArray(array);
